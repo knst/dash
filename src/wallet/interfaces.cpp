5df35ba52f22f04eed66a56a4714201c74b4d30e
Andrew Chow <achow101-github@achow101.com>
2019-08-14 14:25:53 -0400

    Add IsLegacy to CWallet so that the GUI knows whether to show watchonly

--- src/wallet/interfaces.cpp
+++ src/wallet/interfaces.cpp
@@ -556,6 +556,7 @@ public:
     {
         RemoveWallet(m_wallet, false /* load_on_start */);
     }
+    bool isLegacy() override { return m_wallet->IsLegacy(); }
     std::unique_ptr<Handler> handleUnload(UnloadFn fn) override
     {
         return MakeHandler(m_wallet->NotifyUnload.connect(fn));
