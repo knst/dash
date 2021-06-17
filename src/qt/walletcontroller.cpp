424c04ab9c90aeac6539cdbb75ca5216eae317d4
fanquake <fanquake@gmail.com>
2021-06-17 12:15:29 +0800

    Merge bitcoin/bitcoin#21935: Enable external signer support by default, reduce #ifdef
    2f5bdcbc31a2eeb7c11226a9e51c56f02ac807dd gui: misc external signer fixes and translation hints (Sjors Provoost)
    d672404466204444a1d9f2d3498de4448f53d2be refactor: make ExternalSigner NetworkArg() and m_chain private (Sjors Provoost)
    4455145e266450397b45acd7286686966edd072b refactor: reduce #ifdef ENABLE_EXTERNAL_SIGNER usage (Sjors Provoost)
    5be90c907eba0a38019c7d9826623d5d5f567c66 build: enable external signer by default (Sjors Provoost)
    7d9453041b827bafbdfc1ac0b01c7b7e1ee2bd4f refactor: clean up external_signer.h includes (Sjors Provoost)
    fc0eca31b33f87882e2aa329a3746d4e08af1985 fuzz: fix fuzz binary linking order (Sjors Provoost)
    
    Pull request description:
    
      This follows the introduction of GUI support in https://github.com/bitcoin-core/gui/pull/4
    
      I don't think we should expect GUI users to self compile. This also enables external signer support by default for RPC users.
    
      In addition this PR reduces the number of `#ifdef ENABLE_EXTERNAL_SIGNER`, which also fixes #21919. When compiled with `--disable-external-signer` such wallets can't be created in RPC or GUI, but they can be loaded. Attempting any action that calls HWI will trigger an error.
    
      Side-note: this PR may or may not (currently) break CI for the GUI repository, as explained here: https://github.com/bitcoin-core/gui/pull/4#issuecomment-769859001
    
    ACKs for top commit:
      achow101:
        ACK 2f5bdcbc31a2eeb7c11226a9e51c56f02ac807dd
      hebasto:
        re-ACK 2f5bdcbc31a2eeb7c11226a9e51c56f02ac807dd
    
    Tree-SHA512: 1b71c5a8bea2be077ee9fa33a01130c957a0cf90951d4b7b04d3d0ef826bb77e474c3963abddfef2e2c1ea99d9c72cd2302d1eb9b5fcb7ba0bd2a625f006aa05

--- src/qt/walletcontroller.cpp
+++ src/qt/walletcontroller.cpp
@@ -13,6 +13,7 @@
 
 #include <coinjoin/client.h>
 #include <node/context.h>
+#include <external_signer.h>
 #include <interfaces/handler.h>
 #include <interfaces/node.h>
 #include <util/string.h>
@@ -291,7 +292,6 @@ void CreateWalletActivity::create()
 {
     m_create_wallet_dialog = new CreateWalletDialog(m_parent_widget);
 
-#ifdef ENABLE_EXTERNAL_SIGNER
     std::vector<ExternalSigner> signers;
     try {
         signers = node().externalSigners();
@@ -299,7 +299,6 @@ void CreateWalletActivity::create()
         QMessageBox::critical(nullptr, tr("Can't list signers"), e.what());
     }
     m_create_wallet_dialog->setSigners(signers);
-#endif
 
     m_create_wallet_dialog->setWindowModality(Qt::ApplicationModal);
     m_create_wallet_dialog->show();
