6a1e5d50266467a4bc5f97636f47efafc64c701b
Andrew Chow <github@achow101.com>
2023-01-10 17:22:21 -0500

    Merge bitcoin/bitcoin#26186: rpc: Sanitize label name in various RPCs with tests
    65e78bda7cd23ad7b6ede63c6bf5ffe6f552c71d test: Invalid label name coverage (Aurèle Oulès)
    552b51e682b5a52d9e2fbe64e44e623451692bd3 refactor: Add sanity checks in LabelFromValue (Aurèle Oulès)
    67e7ba8e1aea58fc864f9bb1fc0e56b70777185e rpc: Sanitize label name in various RPCs (Aurèle Oulès)
    
    Pull request description:
    
      The following RPCs did not sanitize the optional label name:
      - importprivkey
      - importaddress
      - importpubkey
      - importmulti
      - importdescriptors
      - listsinceblock
    
      Thus is was possible to import an address with a label `*` which should not be possible.
      The wildcard label is used for backwards compatibility in the `listtransactions` rpc.
      I added test coverage for these RPCs.
    
    ACKs for top commit:
      ajtowns:
        ACK 65e78bda7cd23ad7b6ede63c6bf5ffe6f552c71d
      achow101:
        ACK 65e78bda7cd23ad7b6ede63c6bf5ffe6f552c71d
      furszy:
        diff ACK 65e78bd
      stickies-v:
        re-ACK 65e78bda7cd23ad7b6ede63c6bf5ffe6f552c71d
      theStack:
        re-ACK 65e78bda7cd23ad7b6ede63c6bf5ffe6f552c71d
    
    Tree-SHA512: ad99f2824d4cfae352166b76da4ca0069b7c2eccf81aaa0654be25bbb3c6e5d6b005d93960f3f4154155f80e12be2d0cebd5529922ae3d2a36ee4eed82440b31

--- src/wallet/rpc/addresses.cpp
+++ src/wallet/rpc/addresses.cpp
@@ -46,9 +46,7 @@ RPCHelpMan getnewaddress()
     }
 
     // Parse the label first so we don't generate a key if there's an error
-    std::string label;
-    if (!request.params[0].isNull())
-        label = LabelFromValue(request.params[0]);
+    const std::string label{LabelFromValue(request.params[0])};
 
     auto op_dest = pwallet->GetNewDestination(label);
     if (!op_dest) {
@@ -117,7 +115,7 @@ RPCHelpMan setlabel()
         throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid Dash address");
     }
 
-    std::string label = LabelFromValue(request.params[1]);
+    const std::string label{LabelFromValue(request.params[1])};
 
     if (pwallet->IsMine(dest)) {
         pwallet->SetAddressBook(dest, label, "receive");
@@ -231,9 +229,7 @@ RPCHelpMan addmultisigaddress()
 
     LOCK2(pwallet->cs_wallet, spk_man.cs_KeyStore);
 
-    std::string label;
-    if (!request.params[2].isNull())
-        label = LabelFromValue(request.params[2]);
+    const std::string label{LabelFromValue(request.params[2])};
 
     int required = request.params[0].getInt<int>();
 
@@ -607,7 +603,7 @@ RPCHelpMan getaddressesbylabel()
 
     LOCK(pwallet->cs_wallet);
 
-    std::string label = LabelFromValue(request.params[0]);
+    const std::string label{LabelFromValue(request.params[0])};
 
     // Find all addresses that have the given label
     UniValue ret(UniValue::VOBJ);
