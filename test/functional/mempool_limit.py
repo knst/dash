d28529c75b63b9425eca55e83658262c97a8d1d8
Konstantin Akimov <github@knstqq.com>
2023-01-17 11:12:14 +0100

    Merge bitcoin/bitcoin#26884: test: wallet: add coverage for `-spendzeroconfchange` setting
    603d29519931e6ab6a58db09b9d3eb9b355fe566 test: wallet: add coverage for `-spendzeroconfchange` setting (Sebastian Falbesoner)
    50112034bcdd177311679610ef2f3c2c42d6819e test: remove `-spendzeroconfchange` setting from mempool_limit.py (Sebastian Falbesoner)
    
    Pull request description:
    
      This PR adds missing test coverage for the `-spendzeroconfchange` setting (in particular the non-default case `=0`). Note that in contrast to the name, the setting does not only apply to change outputs, but in fact to _all_ unconfirmed outputs that we sent to ourselves, i.e. we can trigger the testing path simply with a single recipient address. The first commit removes the setting from the functional test mempool_limit.py, where it doesn't have any effect, since the test was changed to use MiniWallet in commit dddca3899c4738e512313a85aeb006310e34e31f.
    
    ACKs for top commit:
      brunoerg:
        crACK 603d29519931e6ab6a58db09b9d3eb9b355fe566
    
    Tree-SHA512: 15d9c8bd5eb37c6b228bf887eb27debee0a391c82356662785da4553ee2558e611834c3936ef7136812b46f877bab7aa5f3088bbd278b81f296bdda96cc8e1c3
    
    Co-authored-by: MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>

--- test/functional/mempool_limit.py
+++ test/functional/mempool_limit.py
@@ -31,7 +31,6 @@ class MempoolLimitTest(BitcoinTestFramework):
         self.extra_args = [[
             "-datacarriersize=100000",
             "-maxmempool=5",
-            "-spendzeroconfchange=0",
         ]]
         self.supports_cli = False
 
