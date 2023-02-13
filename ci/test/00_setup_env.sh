97c8907d6fd57affcc534af1eb3f6433899249c0
merge-script <90386131+bitcoin-core-merge-script@users.noreply.github.com>
2023-02-13 11:53:50 +0100

    Merge bitcoin/bitcoin#27033: ci: Cache stuff in volumes, not host folders
    BACKPORT NOTE:
    mostly irrelevant, updated some text comments to reduce conflicts in the future
    ----
    fa8e92c022057adcb8b98647bde626ed9c054df2 doc: Update ci docs (721217.xyz)
    5fffff54e9fcf154c722dc421025a567fa0c5c97 ci: Cache stuff in volumes, not host folders (MarcoFalke)
    
    Pull request description:
    
      Storing cached stuff in host system folders may lead to unexpected issues when the ci-built stuff is used for a non-ci build or a ci task leaks into another ci task.
    
    ACKs for top commit:
      john-moffett:
        ACK fa8e92c022057adcb8b98647bde626ed9c054df2
    
    Tree-SHA512: 8b0c9019452fbe507a272c1037c3dce3c178c21f85ab1096ed3372ad9d4b3c7aa27d89e5bf80c9a6260ea652e0268be0cbe61d6a4fcb3add569fa38076d32287

--- ci/test/00_setup_env.sh
+++ ci/test/00_setup_env.sh
@@ -8,7 +8,6 @@ export LC_ALL=C.UTF-8
 
 # The root dir.
 # The ci system copies this folder.
-# This is where the depends build is done.
 BASE_ROOT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../../ >/dev/null 2>&1 && pwd )
 export BASE_ROOT_DIR
 
@@ -57,18 +56,20 @@ export CCACHE_MAXSIZE=${CCACHE_MAXSIZE:-100M}
 export CCACHE_TEMPDIR=${CCACHE_TEMPDIR:-/tmp/.ccache-temp}
 export CCACHE_COMPRESS=${CCACHE_COMPRESS:-1}
 # The cache dir.
-# This folder exists on the ci host and ci guest. Changes are propagated back and forth.
+# This folder exists only on the ci guest, and on the ci host as a volume.
 export CCACHE_DIR=${CCACHE_DIR:-$CACHE_DIR/ccache}
 # ctcache (clang-tidy cache) configuration
 export CTCACHE_DIR=${CTCACHE_DIR:-$CACHE_DIR/ctcache}
 export CTCACHE_MAXSIZE_MB=${CTCACHE_MAXSIZE_MB:-50}
 # The depends dir.
-# This folder exists on the ci host and ci guest. Changes are propagated back and forth.
+# This folder exists only on the ci guest, and on the ci host as a volume.
 export DEPENDS_DIR=${DEPENDS_DIR:-$BASE_ROOT_DIR/depends}
 # Folder where the build result is put (bin and lib).
 export BASE_OUTDIR=${BASE_OUTDIR:-$BASE_SCRATCH_DIR/out/$HOST}
 # Folder where the build is done (dist and out-of-tree build).
 export BASE_BUILD_DIR=${BASE_BUILD_DIR:-$BASE_SCRATCH_DIR/build-ci}
+# The folder for previous release binaries.
+# This folder exists only on the ci guest, and on the ci host as a volume.
 export PREVIOUS_RELEASES_DIR=${PREVIOUS_RELEASES_DIR:-$BASE_ROOT_DIR/releases/$HOST}
 export SDK_URL=${SDK_URL:-https://bitcoincore.org/depends-sources/sdks}
 export DOCKER_PACKAGES=${DOCKER_PACKAGES:-build-essential libtool autotools-dev automake pkg-config bsdmainutils curl ca-certificates ccache python3 rsync git procps}
