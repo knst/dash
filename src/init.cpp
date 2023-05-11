986260239db04fe70ae9b9a1e1dfd018d8ef435a
Konstantin Akimov <knstqq@gmail.com>
2023-05-12 03:39:23 +0700

    rid of const references to std::unique_ptr from CCoinJoinClientQueueManager and related classes

--- src/init.cpp
+++ src/init.cpp
@@ -2325,7 +2325,7 @@ bool AppInitMain(const CoreContext& context, NodeContext& node, interfaces::Bloc
     ::coinJoinServer = std::make_unique<CCoinJoinServer>(*node.mempool, *node.connman, *::masternodeSync);
 #ifdef ENABLE_WALLET
     if (!ignores_incoming_txs) {
-        ::coinJoinClientQueueManager = std::make_unique<CCoinJoinClientQueueManager>(*node.connman, ::masternodeSync);
+        ::coinJoinClientQueueManager = std::make_unique<CCoinJoinClientQueueManager>(*node.connman, *::masternodeSync);
     }
 #endif // ENABLE_WALLET
 
