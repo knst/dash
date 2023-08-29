nunchuk-io/bitcoin

Implement ReturnDestination in DescriptorScriptPubKeyMan

src/wallet/scriptpubkeyman.cpp
@@ -1607,6 +1607,13 @@ bool DescriptorScriptPubKeyMan::GetReservedDestination(const OutputType type, bo
 
 void DescriptorScriptPubKeyMan::ReturnDestination(int64_t index, bool internal, const CTxDestination& addr)
 {
+    LOCK(cs_desc_man);
+    // Only return when the index was the most recent
+    if (descriptor.next_index - 1 == index) {
+        descriptor.next_index--;
+    }
+    WalletBatch(m_storage.GetDatabase()).WriteDescriptor(GetID(), descriptor);
+    NotifyCanGetAddressesChanged();
 }
 
 std::map<CKeyID, CKey> DescriptorScriptPubKeyMan::GetKeys() const

knst/dash

src/wallet/scriptpubkeyman.cpp
@@ -1882,6 +1882,13 @@ bool DescriptorScriptPubKeyMan::GetReservedDestination(bool internal, CTxDestina
 
 void DescriptorScriptPubKeyMan::ReturnDestination(int64_t index, bool internal, const CTxDestination& addr)
 {
+    LOCK(cs_desc_man);
+    // Only return when the index was the most recent
+    if (m_wallet_descriptor.next_index - 1 == index) {
+        m_wallet_descriptor.next_index--;
+    }
+    WalletBatch(m_storage.GetDatabase()).WriteDescriptor(GetID(), m_wallet_descriptor);
+    NotifyCanGetAddressesChanged();
 }
 
 std::map<CKeyID, CKey> DescriptorScriptPubKeyMan::GetKeys() const