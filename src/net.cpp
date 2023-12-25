16be3d6d0000181ba6545c89e97d6d00d1a578c2
Konstantin Akimov <knstqq@gmail.com>
2023-12-25 20:12:16 +0700

    refactor: use RAII to insead manual memory allocation in src/net.cpp
    We can be a friend with Stroustrup again. This code does same

--- src/net.cpp
+++ src/net.cpp
@@ -3291,8 +3291,7 @@ void CExplicitNetCleanup::callCleanup()
 {
     // Explicit call to destructor of CNetCleanup because it's not implicitly called
     // when the wallet is restarted from within the wallet itself.
-    CNetCleanup *tmp = new CNetCleanup();
-    delete tmp; // Stroustrup's gonna kill me for that
+    CNetCleanup tmp;
 }
 
 void CConnman::Interrupt()
