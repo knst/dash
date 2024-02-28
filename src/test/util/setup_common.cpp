6d51a8c4dae61d62caeba35735252b12bf693322
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2024-02-28 17:16:12 +0000

    refactor: remove CGovernanceManager global, move to NodeContext

--- src/test/util/setup_common.cpp
+++ src/test/util/setup_common.cpp
@@ -221,8 +221,7 @@ ChainTestingSetup::ChainTestingSetup(const std::string& chainName, const std::ve
 
     m_node.netfulfilledman = std::make_unique<CNetFulfilledRequestManager>();
     m_node.sporkman = std::make_unique<CSporkManager>();
-    ::governance = std::make_unique<CGovernanceManager>(*m_node.netfulfilledman);
-    m_node.govman = ::governance.get();
+    m_node.govman = std::make_unique<CGovernanceManager>(*m_node.netfulfilledman);
     ::masternodeSync = std::make_unique<CMasternodeSync>(*m_node.connman, *m_node.netfulfilledman, *m_node.govman);
     m_node.mn_sync = ::masternodeSync.get();
     ::mmetaman = std::make_unique<CMasternodeMetaMan>(/* load_cache */ false);
@@ -244,8 +243,7 @@ ChainTestingSetup::~ChainTestingSetup()
     ::mmetaman.reset();
     m_node.mn_sync = nullptr;
     ::masternodeSync.reset();
-    m_node.govman = nullptr;
-    ::governance.reset();
+    m_node.govman.reset();
     m_node.sporkman.reset();
     m_node.netfulfilledman.reset();
     m_node.connman.reset();
