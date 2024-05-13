2d8992ba7476e3f54f7ac197927aae67ad7d5c59
Konstantin Akimov <knstqq@gmail.com>
2024-05-14 00:05:12 +0700

    dashify src/wallet/rpcwallet.cpp

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -4595,7 +4595,7 @@ static RPCHelpMan walletdisplayaddress()
     return RPCHelpMan{"walletdisplayaddress",
         "Display address on an external signer for verification.",
         {
-            {"address",     RPCArg::Type::STR, RPCArg::Optional::NO, /* default_val */ "", "bitcoin address to display"},
+            {"address",     RPCArg::Type::STR, RPCArg::Optional::NO, /* default_val */ "", "dash address to display"},
         },
         RPCResult{
             RPCResult::Type::OBJ,"","",
