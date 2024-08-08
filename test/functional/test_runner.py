719e87da8e9b63941e7236d30ff00055dde22f48
Konstantin Akimov <knstqq@gmail.com>
2024-08-08 11:42:21 +0700

    fix: move governance higher because that's slower than 60seconds (due to DIP0008 activation)

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -133,6 +133,8 @@ BASE_SCRIPTS = [
     'feature_dip4_coinbasemerkleroots.py', # NOTE: needs dash_hash to pass
     'feature_asset_locks.py', # NOTE: needs dash_hash to pass
     'feature_mnehf.py', # NOTE: needs dash_hash to pass
+    'feature_governance.py --legacy-wallet',
+    'feature_governance.py --descriptors',
     # vv Tests less than 60s vv
     'p2p_sendheaders.py', # NOTE: needs dash_hash to pass
     'p2p_sendheaders_compressed.py', # NOTE: needs dash_hash to pass
@@ -289,8 +291,6 @@ BASE_SCRIPTS = [
     'feature_cltv.py',
     'feature_new_quorum_type_activation.py',
     'feature_governance_objects.py',
-    'feature_governance.py --legacy-wallet',
-    'feature_governance.py --descriptors',
     'feature_governance_cl.py --legacy-wallet',
     'feature_governance_cl.py --descriptors',
     'rpc_uptime.py',
