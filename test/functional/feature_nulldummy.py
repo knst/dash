hebasto/bitcoin-qtw

Introduce -testactivationheight=name@height setting

test/functional/feature_nulldummy.py
@@ -52,7 +52,7 @@ class NULLDUMMYTest(BitcoinTestFramework):
         # This script tests NULLDUMMY activation, which is part of the 'segwit' deployment, so we go through
         # normal segwit activation here (and don't use the default always-on behaviour).
         self.extra_args = [[
-            f'-segwitheight={COINBASE_MATURITY + 5}',
+            f'-testactivationheight=segwit@{COINBASE_MATURITY + 5}',
             '-addresstype=legacy',
             '-par=1',  # Use only one script thread to get the exact reject reason for testing
         ]]

knst/dash

test/functional/feature_nulldummy.py
@@ -50,7 +50,7 @@ class NULLDUMMYTest(BitcoinTestFramework):
         self.extra_args = [[
             '-whitelist=127.0.0.1',
             '-dip3params=105:105',
-            '-bip147height=105',
+            f'-testactivationheight=bip147@{COINBASE_MATURITY + 5}',
             '-par=1',  # Use only one script thread to get the exact reject reason for testing
         ]] * 2
 