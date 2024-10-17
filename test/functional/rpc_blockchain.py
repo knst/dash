bitcoin/bitcoin

Merge bitcoin/bitcoin#23086: test: Add -testactivationheight tests to rpc_blockchain
fa4ca8d5797adb9ad6aa0f150a5f187ebfb714c7 test: Add -testactivationheight tests to rpc_blockchain (MarcoFalke)

Pull request description:

  Suggested: https://github.com/bitcoin/bitcoin/pull/22818#discussion_r712513991

ACKs for top commit:
  laanwj:
    Code review ACK fa4ca8d5797adb9ad6aa0f150a5f187ebfb714c7
  theStack:
    Concept and code-review ACK fa4ca8d5797adb9ad6aa0f150a5f187ebfb714c7

Tree-SHA512: 41304db1a15c0c705a9cc2808c9f1d7831a321a8a7948a28ec5d3ee1ed3da6a0ce67cd50c99a33aaed86830c59608eb6ffadbeaba67d95245c490f9b6c277912

test/functional/rpc_blockchain.py
@@ -126,7 +126,29 @@ class BlockchainTest(SyscoinTestFramework):
         # should have exact keys
         assert_equal(sorted(res.keys()), keys)
 
-        self.restart_node(0, ['-stopatheight=207', '-prune=550'])
+        self.stop_node(0)
+        self.nodes[0].assert_start_raises_init_error(
+            extra_args=['-testactivationheight=name@2'],
+            expected_msg='Error: Invalid name (name@2) for -testactivationheight=name@height.',
+        )
+        self.nodes[0].assert_start_raises_init_error(
+            extra_args=['-testactivationheight=bip34@-2'],
+            expected_msg='Error: Invalid height value (bip34@-2) for -testactivationheight=name@height.',
+        )
+        self.nodes[0].assert_start_raises_init_error(
+            extra_args=['-testactivationheight='],
+            expected_msg='Error: Invalid format () for -testactivationheight=name@height.',
+        )
+        self.start_node(0, extra_args=[
+            '-stopatheight=207',
+            '-prune=550',
+            '-testactivationheight=bip34@2',
+            '-testactivationheight=dersig@3',
+            '-testactivationheight=cltv@4',
+            '-testactivationheight=csv@5',
+            '-testactivationheight=segwit@6',
+        ])
+
         res = self.nodes[0].getblockchaininfo()
         # result should have these additional pruning keys if prune=550
         assert_equal(sorted(res.keys()), sorted(['pruneheight', 'automatic_pruning', 'prune_target_size'] + keys))
@@ -138,11 +160,11 @@ class BlockchainTest(SyscoinTestFramework):
         assert_equal(res['prune_target_size'], 576716800)
         assert_greater_than(res['size_on_disk'], 0)
         assert_equal(res['softforks'], {
-            'bip34': {'type': 'buried', 'active': True, 'height': 1},
-            'bip66': {'type': 'buried', 'active': True, 'height': 1},
-            'bip65': {'type': 'buried', 'active': True, 'height': 1},
-            'csv': {'type': 'buried', 'active': True, 'height': 1},
-            'segwit': {'type': 'buried', 'active': True, 'height': 1},
+            'bip34': {'type': 'buried', 'active': True, 'height': 2},
+            'bip66': {'type': 'buried', 'active': True, 'height': 3},
+            'bip65': {'type': 'buried', 'active': True, 'height': 4},
+            'csv': {'type': 'buried', 'active': True, 'height': 5},
+            'segwit': {'type': 'buried', 'active': True, 'height': 6},
             'testdummy': {
                 'type': 'bip9',
                 'bip9': {

knst/dash

test/functional/rpc_blockchain.py
@@ -147,7 +147,37 @@ class BlockchainTest(BitcoinTestFramework):
         # should have exact keys
         assert_equal(sorted(res.keys()), keys)
 
-        self.restart_node(0, ['-stopatheight=207', '-prune=550', '-txindex=0'])
+        self.stop_node(0)
+        self.nodes[0].assert_start_raises_init_error(
+            extra_args=['-testactivationheight=name@2'],
+            expected_msg='Error: Invalid name (name@2) for -testactivationheight=name@height.',
+        )
+        self.nodes[0].assert_start_raises_init_error(
+            extra_args=['-testactivationheight=bip34@-2'],
+            expected_msg='Error: Invalid height value (bip34@-2) for -testactivationheight=name@height.',
+        )
+        self.nodes[0].assert_start_raises_init_error(
+            extra_args=['-testactivationheight='],
+            expected_msg='Error: Invalid format () for -testactivationheight=name@height.',
+        )
+        self.start_node(0, extra_args=[
+            '-stopatheight=207',
+            '-prune=550',
+            '-txindex=0',
+            '-testactivationheight=bip34@2',
+            '-testactivationheight=dersig@3',
+            '-testactivationheight=cltv@4',
+            '-testactivationheight=csv@5',
+            '-testactivationheight=bip147@6',
+            '-testactivationheight=dip0001@10',
+            '-dip3params=411:511',
+            '-testactivationheight=dip0008@12',
+            '-testactivationheight=dip0024@13',
+            '-testactivationheight=brr@14',
+            '-testactivationheight=v20@901',
+            '-testactivationheight=mn_rr@902',
+        ])
+
         res = self.nodes[0].getblockchaininfo()
         # result should have these additional pruning keys if prune=550
         assert_equal(sorted(res.keys()), sorted(['pruneheight', 'automatic_pruning', 'prune_target_size'] + keys))
@@ -159,20 +189,20 @@ class BlockchainTest(BitcoinTestFramework):
         assert_equal(res['prune_target_size'], 576716800)
         assert_greater_than(res['size_on_disk'], 0)
         assert_equal(res['softforks'], {
-            'bip34': {'type': 'buried', 'active': True, 'height': 1},
-            'bip66': {'type': 'buried', 'active': True, 'height': 1},
-            'bip65': {'type': 'buried', 'active': True, 'height': 1},
-            'bip147': { 'type': 'buried', 'active': True, 'height': 1},
-            'csv': {'type': 'buried', 'active': True, 'height': 1},
-            'dip0001': { 'type': 'buried', 'active': True, 'height': 1},
-            'dip0003': { 'type': 'buried', 'active': False, 'height': 432},
-            'dip0008': { 'type': 'buried', 'active': True, 'height': 1},
+            'bip34': {'type': 'buried', 'active': True, 'height': 2},
+            'bip66': {'type': 'buried', 'active': True, 'height': 3},
+            'bip65': {'type': 'buried', 'active': True, 'height': 4},
+            'csv': {'type': 'buried', 'active': True, 'height': 5},
+            'bip147': {'type': 'buried', 'active': True, 'height': 6},
+            'dip0001': { 'type': 'buried', 'active': True, 'height': 10},
+            'dip0003': { 'type': 'buried', 'active': False, 'height': 411},
+            'dip0008': { 'type': 'buried', 'active': True, 'height': 12},
             'dip0020': { 'type': 'buried', 'active': True, 'height': 1},
-            'dip0024': { 'type': 'buried', 'active': True, 'height': 1},
-            'realloc': { 'type': 'buried', 'active': True, 'height': 1},
+            'dip0024': { 'type': 'buried', 'active': True, 'height': 13},
+            'realloc': { 'type': 'buried', 'active': True, 'height': 14},
             'v19': { 'type': 'buried', 'active': False, 'height': 900},
-            'v20': { 'type': 'buried', 'active': False, 'height': 900},
-            'mn_rr': { 'type': 'buried', 'active': False, 'height': 900},
+            'v20': { 'type': 'buried', 'active': False, 'height': 901},
+            'mn_rr': { 'type': 'buried', 'active': False, 'height': 902},
             'testdummy': {
                 'type': 'bip9',
                 'bip9': {