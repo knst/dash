09af7b78c9f46071e1f5b5d22233a0b6d1207680
Konstantin Akimov <knstqq@gmail.com>
2024-12-26 12:46:53 +0700

    Revert "Merge #6383: backport: merge bitcoin#28370, #28759, #28580, #28786, #29078, #27897, #29651, #29695, #29673, #29828, #29846, #30231, #30438, partial bitcoin#30511 (guix backports: part 5)"
    This reverts commit be97bfeedc7f22345f32dfca0f685d8796bb1c8f, reversing
    changes made to f155ecfa29de98063786f84fea391a2bc973265e.

--- contrib/guix/libexec/prelude.bash
+++ contrib/guix/libexec/prelude.bash
@@ -51,7 +51,7 @@ fi
 time-machine() {
     # shellcheck disable=SC2086
     guix time-machine --url=https://git.savannah.gnu.org/git/guix.git \
-                      --commit=f0bb724211872cd6158fce6162e0b8c73efed126 \
+                      --commit=160f78a4d92205df986ed9efcce7d3aac188cb24 \
                       --cores="$JOBS" \
                       --keep-failed \
                       --fallback \
