darkcoin/darkcoin

refactor: rename SetByteVector -> SetBytes

src/llmq/dkgsession.cpp
@@ -1018,12 +1018,12 @@ void CDKGSession::SendCommitment(CDKGPendingMessages& pendingMessages, PeerManag
         const bool is_bls_legacy = bls::bls_legacy_scheme.load();
         std::vector<uint8_t> buf = qc.sig.ToByteVector(is_bls_legacy);
         buf[5]++;
-        qc.sig.SetByteVector(buf, is_bls_legacy);
+        qc.sig.SetBytes(buf, is_bls_legacy);
     } else if (lieType == 4) {
         const bool is_bls_legacy = bls::bls_legacy_scheme.load();
         std::vector<uint8_t> buf = qc.quorumSig.ToByteVector(is_bls_legacy);
         buf[5]++;
-        qc.quorumSig.SetByteVector(buf, is_bls_legacy);
+        qc.quorumSig.SetBytes(buf, is_bls_legacy);
     }
 
     t3.stop();

knst/dash

src/llmq/dkgsession.cpp
@@ -1021,11 +1021,11 @@ void CDKGSession::SendCommitment(CDKGPendingMessages& pendingMessages, PeerManag
     if (lieType == 3) {
         std::vector<uint8_t> buf = qc.sig.ToByteVector(m_use_legacy_bls);
         buf[5]++;
-        qc.sig.SetByteVector(buf, m_use_legacy_bls);
+        qc.sig.SetBytes(buf, m_use_legacy_bls);
     } else if (lieType == 4) {
         std::vector<uint8_t> buf = qc.quorumSig.ToByteVector(m_use_legacy_bls);
         buf[5]++;
-        qc.quorumSig.SetByteVector(buf, m_use_legacy_bls);
+        qc.quorumSig.SetBytes(buf, m_use_legacy_bls);
     }
 
     t3.stop();