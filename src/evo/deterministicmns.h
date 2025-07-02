d85d4b7bdfacedb58e822567316f18f98500a2b5
Konstantin Akimov <knstqq@gmail.com>
2025-07-02 12:54:12 +0700

    Revert "perf: use std::map instead of immer::map"
    This reverts commit a73e394ec8560cd26b025f8de717be1e12dbca49.

--- src/evo/deterministicmns.h
+++ src/evo/deterministicmns.h
@@ -137,8 +137,7 @@ private:
     };
 
 public:
-//    using MnMap = immer::map<uint256, CDeterministicMNCPtr, ImmerHasher>;
-    using MnMap = std::map<uint256, CDeterministicMNCPtr>;
+    using MnMap = immer::map<uint256, CDeterministicMNCPtr, ImmerHasher>;
     using MnInternalIdMap = immer::map<uint64_t, uint256>;
     using MnUniquePropertyMap = immer::map<uint256, std::pair<uint256, uint32_t>, ImmerHasher>;
 
