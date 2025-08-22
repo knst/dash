6081087610a7375b73110b6a7399fcdaddfb0473
Konstantin Akimov <knstqq@gmail.com>
2025-08-22 14:34:48 +0700

    test: generate less blocks to activate mn_rr for feature_llmq_evo.py

--- test/functional/feature_llmq_evo.py
+++ test/functional/feature_llmq_evo.py
@@ -50,7 +50,7 @@ class TestP2PConn(P2PInterface):
 class LLMQEvoNodesTest(DashTestFramework):
     def set_test_params(self):
         self.set_dash_test_params(4, 3, evo_count=5)
-        self.mn_rr_height = 400
+        self.mn_rr_height = 360
 
     def run_test(self):
         # Connect all nodes to node1 so that we always have the whole network connected
