115c72a68eade7e15c5bbcc5ae20a90d1395f937
Konstantin Akimov <knstqq@gmail.com>
2025-10-05 03:49:41 +0700

    Revert "Merge bitcoin-core/gui#613: Remove unnecessary wallet includes from rpcconsole.cpp"
    This reverts commit 92a0becd90a17c1a80af03ca7ceb85197ea33fd8.

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -30,6 +30,14 @@
 
 #include <univalue.h>
 
+#ifdef ENABLE_WALLET
+#ifdef USE_BDB
+#include <wallet/bdb.h>
+#endif
+#include <wallet/db.h>
+#include <wallet/walletutil.h>
+#endif
+
 #include <QAbstractButton>
 #include <QAbstractItemModel>
 #include <QButtonGroup>
