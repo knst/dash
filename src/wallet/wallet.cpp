bb6a79a7ca39b22ad11f919e180520c8f19a52fe
Konstantin Akimov <github@knstqq.com>
2026-04-09 21:35:30 +0700

    fix: follow-up backporting bitcoin#17261 - missing break

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3242,6 +3242,7 @@ std::shared_ptr<CWallet> CWallet::Create(WalletContext& context, const std::stri
         for (auto spk_man : walletInstance->GetActiveScriptPubKeyMans()) {
             if (spk_man->HavePrivateKeys()) {
                 warnings.push_back(strprintf(_("Warning: Private keys detected in wallet {%s} with disabled private keys"), walletFile));
+                break;
             }
         }
     }
