bitcoin-ghost/bitcoin-core-ladder-script

refactor: wallet, remove global 'ArgsManager' access
we are not using it anymore

src/wallet/test/util.h
@@ -21,7 +21,7 @@ class CWallet;
 struct DatabaseOptions;
 class WalletDatabase;
 
-std::unique_ptr<CWallet> CreateSyncedWallet(interfaces::Chain& chain, CChain& cchain, ArgsManager& args, const CKey& key);
+std::unique_ptr<CWallet> CreateSyncedWallet(interfaces::Chain& chain, CChain& cchain, const CKey& key);
 
 // Creates a copy of the provided database
 std::unique_ptr<WalletDatabase> DuplicateMockDatabase(WalletDatabase& database, DatabaseOptions& options);

knst/dash

src/wallet/test/util.h
@@ -25,7 +25,7 @@ class CWallet;
 struct DatabaseOptions;
 class WalletDatabase;
 
-std::unique_ptr<CWallet> CreateSyncedWallet(interfaces::Chain& chain, ChainstateManager& chainman, ArgsManager& args, const CKey& key);
+std::unique_ptr<CWallet> CreateSyncedWallet(interfaces::Chain& chain, ChainstateManager& chainman, const CKey& key);
 
 // Creates a copy of the provided database
 std::unique_ptr<WalletDatabase> DuplicateMockDatabase(WalletDatabase& database, DatabaseOptions& options);